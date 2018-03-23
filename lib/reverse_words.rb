632e4ad638528c558404814da4c733fac77f8cf6
Abiaina <abinnet.a@gmail.com>
2018-03-22 18:30:06 -0700

    one test passing, individual words reverse but the word order in the original string is not respected

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,4 +1,37 @@
-# A method to reverse each word in a sentence, in place.
+require 'pry'
+
 def reverse_words(my_words)
-  raise NotImplementedError
+
+  index = 0
+  rev_first = 0
+
+  if my_words.length < 1
+    return my_words
+  end
+
+  my_words.length.times do
+    if my_words[index] == " " || index = ((my_words.length) -1)
+
+      rev_last = index
+
+      while rev_first - rev_last <= 0
+        first = my_words[rev_first]
+        my_words[rev_first] = my_words[rev_last]
+        my_words[rev_last] = first
+
+        rev_first += 1
+        rev_last -= 1
+      end
+    end
+    index += 1
+    rev_first = index
+  end
+  return my_words
 end
+
+
+
+
+puts reverse_words("hello me")
+
+#olehh
