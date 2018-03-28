afb63ed7c9e9b2dff2bbaac552946f12473d38bb
Abiaina <abinnet.a@gmail.com>
2018-03-28 16:51:15 -0700

    rewrote entire code, all tests fail/error message out

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -1,43 +1,31 @@
 require 'pry'
 
-def string_reverse(string, index_f, index_l)
-
-  while index_f - index_l <= 0
-    first = string[index_f]
-    last = string[index_l]
-    string[index_f] = last
-    string[index_l] = first
-
-    index_f += 1
-    index_l -= 1
-  end
-end
-
-def reverse_words(my_words)
+def reverse_words(string)
   index = 0
-  rev_first = 0
+  first_index = 0
 
-  if my_words.length < 1
-    return my_words
-  end
-
-  my_words.length.times do
-    if my_words[index] == " " || index = ((my_words.length))
-
-      if my_words[index] = " "
-        rev_last = index - 1
-      else
-        rev_last = index
-      end
-
-      string_reverse(my_words, rev_first, rev_last)
-binding.pry
-      rev_first = index + 1
+  until string[index] == " " || index == (string.length - 1)
+    if string[index] == " "
+      last_index = index - 1
     end
+
+    ((last_index - first_index) / 2).times do
+      first = string[first_index]
+      string[first_index] = string[last_index]
+      string[last_index] = first
+
+      first_index += 1
+      last_index -= 1
+    end
+
+
+    until string[index] != " "
+      index += 1
+    end
+
+    first_index = index
+
     index += 1
   end
-
-  return my_words
+  return string
 end
-
-puts reverse_words("hello me")
